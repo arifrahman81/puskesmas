96fbec4bb7d4723e00ce030bec15c59a14947e08
arifrahman81 <wwwlcfcom81@gmail.com>
2023-11-14 21:23:32 +0800

    selesai

--- app/Views/dinkes/data_penyakit.php
+++ app/Views/dinkes/data_penyakit.php
@@ -10,13 +10,23 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable1" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
+                <tr>
                             <th>No.</th>
+                            <th>Tahun</th>
+                            <th>Bulan</th>
+                            <th>Provinsi</th>
                             <th>Kota</th>
                             <th>Puskesmas</th>
-                            <th>Judul Laporan</th>
-                            <th>Isi Laporan</th>
+                            <th>NIK</th>
+                            <th>Nama Penderita</th>
+                            <th>Umur</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Nama Wali</th>
+                            <th>Alamat</th>
+                            <th>Tgl Kunjungan</th>
+                            <th>Diagnosa</th>
+                            <th>Klinis +</th>
+                            <th>Laboratorium +</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -24,14 +34,24 @@
                         <?php foreach ($data as $data) : ?>
                             <tr>
                                 <td><?= $no++ ?> </td>
+                                <td><?= $data['tahun'] ?> </td>
+                                <td><?= $data['bulan'] ?> </td>
+                                <td><?= $data['provinsi'] ?> </td>
                                 <td><?= $data['kota'] ?> </td>
-                                <td><?= $data['nama_puskesmas'] ?> </td>
-                                <td><?= $data['judul_laporan'] ?> </td>
-                                <td>
-                                    <a class="btn btn-info" href="/file/download_laporan_puskesmas/<?= $data['id']; ?>">Download</a>
-                                </td>
+                                <td><?= $data['puskesmas'] ?> </td>
+                                <td><?= $data['nik'] ?> </td>
+                                <td><?= $data['nama_penderita'] ?> </td>
+                                <td><?= $data['umur'] ?> </td>
+                                <td><?= $data['jenis_kelamin'] ?> </td>
+                                <td><?= $data['nama_wali'] ?> </td>
+                                <td><?= $data['alamat'] ?> </td>
+                                <td><?= $data['tgl_kunjungan'] ?> </td>
+                                <td><?= $data['diagnosa'] ?> </td>
+                                <td><?= $data['klinis'] ?> </td>
+                                <td><?= $data['laboratorium'] ?> </td>
                             </tr>
                         <?php endforeach ?>
+
                     </tbody>
                 </table>
             </div>
